230a7b75ec55e3c8f569c019e3dc9ede59f60def
jacob-bonner <54957118+jacob-bonner@users.noreply.github.com>
2019-11-18 14:23:21 -0500

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -11,7 +11,7 @@ def calculate(base, height):
     # Process
     area = (base*height)/2
     print("")
-    print("The area of the triangle is {0}cm2".format(area))
+    print("The area of the triangle is {0}cm^2".format(area))
 
 
 def main():
@@ -20,14 +20,15 @@ def main():
     # Process
     while True:
         # Input
-        base = input("Enter the base in cm here (integer): ")
-        height = input("Enter the height in cm here (integer): ")
+        base_from_user = input("Enter the base in cm here (integer): ")
+        height_from_user = input("Enter the height in cm here (integer): ")
 
         try:
-            base = int(base)
-            height = int(height)
-            calculate(base, height)
-            if base == int(base) and height == int(height):
+            base_from_user = int(base_from_user)
+            height_from_user = int(height_from_user)
+            calculate(base_from_user, height_from_user)
+            if base_from_user == int(base_from_user) and \
+               height_from_user == int(height_from_user):
                 break
             else:
                 print("Error, unable to process inputs")
